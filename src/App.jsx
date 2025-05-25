c2839907eac1c71b76dbec0e8d233443ae6ec5e8
MUHAMMAD WAQAS <141298724+Muhammadwaqas1234@users.noreply.github.com>
2025-05-25 20:23:22 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 function App() {
-  return <Layout />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
